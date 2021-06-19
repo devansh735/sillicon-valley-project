2f6340ae8574a357fc03705eab27d9b409f1a575
devansh735 <74194267+devansh735@users.noreply.github.com>
2021-06-19 11:43:08 +0530

    Add files via upload

---
+++ fabric.js
@@ -0,0 +1,23 @@
+function next(){
+    window.location="information.html";
+}
+
+function previous(){
+    window.location="index.html";
+}
+
+function next2(){
+    window.location="more.html";
+}
+function next3(){
+    window.location="more.html";
+}
+function next4(){
+    window.location="index.html";
+}
+function button_6(){
+    window.location="video.html";
+}
+function button_7(){
+    window.location="information.html";
+}
